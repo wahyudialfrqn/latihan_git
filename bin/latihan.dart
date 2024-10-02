2e07a09b6bc172fcde5ea65259b46e4685783b4c
Wahyudi Alfurqon <wahyudialfurqon8@gmail.com>
2024-10-02 10:59:18 +0700

    Update Latihan

--- bin/latihan.dart
+++ bin/latihan.dart
@@ -1,5 +1,3 @@
-import 'package:latihan/latihan.dart' as latihan;
-
-void main(List<String> arguments) {
-  print('Hello world: ${latihan.calculate()}!');
+void main() {
+  print("Hellow World");
 }
